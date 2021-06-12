943d8d1db4923a4db3893831566194d9f5980183
Arthur Konovalov <dinozyavier@outlook.com>
2021-06-12 16:05:49 +0300

    Refactor and add title
    Signed-off-by: Arthur Konovalov <dinozyavier@outlook.com>

--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -1,15 +1,16 @@
 #include "include/mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow( QWidget* parent )
+    : QMainWindow( parent )
+    , m_ui( new Ui::MainWindow )
 {
-    ui->setupUi(this);
+    m_ui->setupUi(this);
+    setWindowTitle( tr( "Project Manager" ) );
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+    delete m_ui;
 }
 
